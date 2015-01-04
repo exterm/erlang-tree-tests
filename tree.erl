94ce5a8f239e20fbe2a006c76ed83580044b82f7
Philip Mueller <mail@philip.in-aachen.net>
2015-01-04 21:34:34 +0100

    more fun()

--- tree.erl
+++ tree.erl
@@ -1,15 +1,23 @@
 -module(tree).
 
--export([is_elem/2, example_tree/0, test/0]).
+-export([is_elem/2, example_tree/0, test/0, zip_with/3]).
 
 -type non_empty_tree(ElType) :: {Value :: ElType,
                                  Left  :: tree(ElType),
                                  Right :: tree(ElType)}.
 -type tree(ElType) :: nothing | non_empty_tree(ElType).
 
+-spec is_elem(V, tree(V)) -> boolean().
 is_elem(_Elem, nothing)              -> false;
 is_elem(Elem, {Elem, _Left, _Right}) -> true;
 is_elem(Elem, {_, Left, Right})      -> is_elem(Elem, Left) or is_elem(Elem, Right).
+
+% trees need to have the same structure, type system can not express that
+-spec zip_with(fun((A,B) -> C), tree(A), tree(B)) -> tree(C).
+zip_with(_, nothing, nothing) ->
+    nothing;
+zip_with(F, {E1, Left1, Right1}, {E2, Left2, Right2}) ->
+    {F(E1,E2), zip_with(F, Left1, Left2), zip_with(F, Right1, Right2)}.
 
 -spec example_tree() -> non_empty_tree(integer()).
 example_tree() ->
@@ -27,4 +35,7 @@ test() ->
     false = is_elem(6, example_tree()),
     true = is_elem(5, example_tree()),
     true = is_elem(1, example_tree()),
+    DoubledTree = zip_with(fun(A,B) -> A+B end, example_tree(), example_tree()),
+    true = is_elem(6, DoubledTree),
+    false = is_elem(1, DoubledTree),
     ok.
